44cb88f5941e99ba215ba70d1913499977bd637d
Muklis12133 <muklis6763@gmail.com>
2022-09-26 03:02:34 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 ┌─「 Donasi • Pulsa 」
-│ • *Telkomsel:* [${global.ppulsa}]
+│ • *Indosat:* [${085838571639}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
-│ • *Dana:* [${global.pdana}]
-│ • *Gopay:* [${global.pgopay}]
-│ • *Ovo:* [${global.povo}]
-│ • *Link Aja:* [${global.plinkaja}]
+│ • *Dana:* [${085896661014}]
+│ • *Gopay:* [${085838571639}]
+│ • *Ovo:* [${08XXXXXXXXXX}]
+│ • *Link Aja:* [${08XXXXXXXXXX}]
 ❏────
 `.trim()
   m.reply(text)
